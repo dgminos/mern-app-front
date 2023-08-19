b1cd91f9ef0aa88414d37291efbb60c78bc7a30b
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2023-08-19 13:03:36 -0300

    remove slash from fl0 backend URL

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -18,7 +18,7 @@ const TaskDetails = ({ task }) => {
   const handleShow = () => setShow(true)
   const handleClose = () => setShow(false)
 
-  const backURL= 'https://mern-todo-app-back-884n-dev.fl0.io/'
+  const backURL= 'https://mern-todo-app-back-884n-dev.fl0.io'
 
   useEffect(() => {
     return () => handleClose()
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const {tasks, dispatch} = useTasksContext()
   const {user} = useAuthContext()
 
-  const backURL= 'https://mern-todo-app-back-884n-dev.fl0.io/'
+  const backURL= 'https://mern-todo-app-back-884n-dev.fl0.io'
 
   useEffect(() => {
     const fetchTasks = async () => {
